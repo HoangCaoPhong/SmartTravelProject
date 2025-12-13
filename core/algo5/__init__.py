3a22aff33c768ac2f12506a95faa2da4b72b9dc3
Nguyễn Trung Kiên <thanhtuankien1503@gmail.com>
2025-12-13 22:25:04 +0700

    modified:   core/algo5/__init__.py 	modified:   pages/__pycache__/page_chuc_nang.cpython-313.pyc

--- core/algo5/__init__.py
+++ core/algo5/__init__.py
@@ -3,7 +3,7 @@ Algo5: Location Suggestions
 Suggests locations based on user preferences
 """
 
-from .recommender import recommend_places
+from core.algo5.recommender import recommend_places
 
 __all__ = ['recommend_places']
 
